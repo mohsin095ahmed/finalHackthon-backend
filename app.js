2d5d5034c7839a9db337c22cf67b2bdaa139ea78
mohsin095ahmed <mohsin095ahmed@gmail.com>
2024-01-22 10:33:56 -0800

    add new cors line

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ import 'dotenv/config';
 const PORT = process.env.PORT || 4000
 
 const app =express();
-app.use(cors());
+var corsOptions = {
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
+app.use(cors(corsOptions));
 app.use(express.json());
 //CONECTION ON MONGODB
 
